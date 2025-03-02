246368e66aa2aa5be985a82a7612d841fca340c8
Vitek <vitekman08@gmail.com>
2025-03-02 09:25:32 +0300

    First commit

---
+++ src/main/java/com/user/subscriptions/apiuser/ApiuserApplication.java
@@ -0,0 +1,13 @@
+package com.user.subscriptions.apiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/subscriptions/apiuser/ApiuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.subscriptions.apiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
